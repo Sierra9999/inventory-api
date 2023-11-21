2ac752f1f6020b745a9a32a34c1a1a0504e49529
Mateo Sierra <m.sierra1037@hotmail.com>
2023-11-20 19:22:22 -0600

    feat: first response with express

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    const data = {
+        name: 'inventory api',
+        description: 'still in progress'
+    };
+    res.json(data);
+});
+app.listen(port, () => {
+    console.log(`server running at port ${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import express, { Request, Response } from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  const data = {
+    name : 'inventory api',
+    description : 'still in progress'
+  }
+  res.json(data)
+})
+
+app.listen(port, () => {
+  console.log(`server running at port ${port}`)
+})
